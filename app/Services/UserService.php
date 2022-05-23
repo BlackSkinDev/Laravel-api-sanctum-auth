b3420a8c6bbad3e18ff159304fa025f0ea193c37
Chilling Loccini <chillingloccini@Chillings-MacBook-Pro.local>
2022-05-23 14:30:45 +0100

    laravel facade implementation

--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -4,6 +4,11 @@ use App\Models\User;
 
 class UserService{
 
+    //create your class
+    // create a facade class which extends Facade (wuth getFacadeAccessor method which returns Service container key)
+    // Bind that key in your App service provider which returns a callback(call back returns your main class)
+    // Facade class is ready to call your main class methods statically. You can create allias in your config/app.php for faster access to facade
+
     public function getUsers(){
         return User::all()->modelKeys();
     }
